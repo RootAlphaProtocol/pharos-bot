8e7e86fc6756b1323bd9e2ee819cec4216f1a17a
Root Alpha Protocol <ridamalik1320@gmail.com>
2025-07-17 22:20:44 +0600

    bot5.py

--- bot5.py
+++ bot5.py
@@ -108,10 +108,10 @@ class Faroswap:
         print(Fore.GREEN + Style.BRIGHT + "    ⚡ Pharos BOT TELEGRAM LINK : https://t.me/RootAlpha_ProtocoL⚡")
         print(Fore.CYAN + Style.BRIGHT + "    ────────────────────────────────")
         print(Fore.YELLOW + Style.BRIGHT + "    🧠 Project    : Faroswap - Automation Bot")
-        print(Fore.YELLOW + Style.BRIGHT + "    🧑‍💻 Author     : ATeeq and sabbir RootAlphaProtocol")
+        print(Fore.YELLOW + Style.BRIGHT + "    🧑‍💻 Author     : GITHUB LINK : https://github.com/RootAlphaProtocol")
         print(Fore.YELLOW + Style.BRIGHT + "    🌐 Status     : Running & Monitering...")
         print(Fore.CYAN + Style.BRIGHT + "    ────────────────────────────────")
-        print(Fore.MAGENTA + Style.BRIGHT + "    🧬 Powered by RootAlphaProtocol × Ateeq and Sabbir | Buddy v1.2 🚀")
+        print(Fore.MAGENTA + Style.BRIGHT + "    🧬 Powered by RootAlphaProtocol × ATEEQ UR REHMAN  🇵🇰  SABBIR AHMED SA  🇧🇩| Buddy v1.2 🚀")
         print(Fore.LIGHTGREEN_EX + Style.BRIGHT + "═" * 60 + "\n")
 
     def format_seconds(self, seconds):
